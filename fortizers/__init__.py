ec039012031e79a183f5c4990a8015fe294116de
Heinz Der Flugel <heinzdf@outlook.com>
2020-10-24 12:42:45 +0800

    Re-implement SpamWatch and rebrand Gban modules
    Also apply enforcing gban
    
    Co-authored-by: あきと <akito@evolution-x.org>
    Signed-off-by: Heinz Der Flugel <heinzdf@outlook.com>

--- fortizers/__init__.py
+++ fortizers/__init__.py
@@ -1,73 +1,74 @@
 import logging
 import os
 import sys
+import spamwatch
 import time
 from datetime import datetime
 from functools import wraps
-
+ 
 import telegram.ext as tg
-
+ 
 # enable logging
 logging.basicConfig(
 	format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 	level=logging.INFO)
-
+ 
 LOGGER = logging.getLogger(__name__)
-
+ 
 # if version < 3.6, stop bot.
 if sys.version_info[0] < 3 or sys.version_info[1] < 6:
 	LOGGER.error("You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting.")
 	quit(1)
-
+ 
 # Check if system is reboot or not
 try:
 	os.remove("reboot")
 except Exception:
 	pass
-
+ 
 ENV = bool(os.environ.get('ENV', False))
-
+ 
 if ENV:
 	TOKEN = os.environ.get('TOKEN', None)
 	try:
 		OWNER_ID = int(os.environ.get('OWNER_ID', None))
 	except ValueError:
 		raise Exception("Your OWNER_ID env variable is not a valid integer.")
-
+ 
 	MESSAGE_DUMP = os.environ.get('MESSAGE_DUMP', None)
 	OWNER_USERNAME = os.environ.get("OWNER_USERNAME", None)
 	IS_DEBUG = os.environ.get("IS_DEBUG", False)
-
+ 
 	try:
 		SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", "").split())
 	except ValueError:
 		raise Exception("Your sudo users list does not contain valid integers.")
-
+ 
 	try:
 		SUPPORT_USERS = set(int(x) for x in os.environ.get("SUPPORT_USERS", "").split())
 	except ValueError:
 		raise Exception("Your support users list does not contain valid integers.")
-
+ 
 	try:
 		SPAMMERS = set(int(x) for x in os.environ.get("SPAMMERS", "").split())
 	except ValueError:
 		raise Exception("Your spammers users list does not contain valid integers.")
-
+ 
 	try:
 		GROUP_BLACKLIST = set(int(x) for x in os.environ.get("GROUP_BLACKLIST", "").split())
 	except ValueError:
 		raise Exception("Your GROUP_BLACKLIST users list does not contain valid integers.")
-
+ 
 	try:
 		WHITELIST_USERS = set(int(x) for x in os.environ.get("WHITELIST_USERS", "").split())
 	except ValueError:
 		raise Exception("Your whitelisted users list does not contain valid integers.")
-
+ 
 	WEBHOOK = bool(os.environ.get('WEBHOOK', False))
 	URL = os.environ.get('URL', "")  # Does not contain token
 	PORT = int(os.environ.get('PORT', 5000))
 	CERT_PATH = os.environ.get("CERT_PATH")
-
+ 
 	DB_URI = os.environ.get('DATABASE_URL')
 	DONATION_LINK = os.environ.get('DONATION_LINK')
 	LOAD = os.environ.get("LOAD", "").split()
@@ -80,14 +81,13 @@ if ENV:
 	CUSTOM_CMD = os.environ.get('CUSTOM_CMD', False)
 	LASTFM_API_KEY = os.environ.get('LASTFM_API_KEY', "")
 	TEMPORARY_DATA = os.environ.get('TEMPORARY_DATA', None)
-	SPAMWATCH_TOKEN = os.environ.get('SPAMWATCH_TOKEN', None)
 	WALL_API = os.environ.get('WALL_API', None)
 	CAT_API_KEY = os.environ.get('CAT_API_KEY', None)
 	DOG_API_KEY = os.environ.get('DOG_API_KEY', None)
 	API_WEATHER = os.environ.get('API_OPENWEATHER', None)
 	TIME_API_KEY = os.environ.get('TIME_API_KEY', None)
 	DEEPFRY_TOKEN = os.environ.get('DEEPFRY_TOKEN', "")
-
+ 
 else:
 	from fortizers.config import Development as Config
 	TOKEN = Config.API_KEY
@@ -95,44 +95,44 @@ else:
 		OWNER_ID = int(Config.OWNER_ID)
 	except ValueError:
 		raise Exception("Your OWNER_ID variable is not a valid integer.")
-
+ 
 	MESSAGE_DUMP = Config.MESSAGE_DUMP
 	OWNER_USERNAME = Config.OWNER_USERNAME
 	try:
 		IS_DEBUG = Config.IS_DEBUG
 	except AttributeError:
 		IS_DEBUG = False
-
+ 
 	try:
 		SUDO_USERS = set(int(x) for x in Config.SUDO_USERS or [])
 	except ValueError:
 		raise Exception("Your sudo users list does not contain valid integers.")
-
+ 
 	try:
 		SUPPORT_USERS = set(int(x) for x in Config.SUPPORT_USERS or [])
 	except ValueError:
 		raise Exception("Your support users list does not contain valid integers.")
-
+ 
 	try:
 		SPAMMERS = set(int(x) for x in Config.SPAMMERS or [])
 	except ValueError:
 		raise Exception("Your spammers users list does not contain valid integers.")
-
+ 
 	try:
 		GROUP_BLACKLIST = set(int(x) for x in Config.GROUP_BLACKLIST or [])
 	except ValueError:
 		raise Exception("Your GROUP_BLACKLIST users list does not contain valid integers.")
-
+ 
 	try:
 		WHITELIST_USERS = set(int(x) for x in Config.WHITELIST_USERS or [])
 	except ValueError:
 		raise Exception("Your whitelisted users list does not contain valid integers.")
-
+ 
 	WEBHOOK = Config.WEBHOOK
 	URL = Config.URL
 	PORT = Config.PORT
 	CERT_PATH = Config.CERT_PATH
-
+ 
 	DB_URI = Config.SQLALCHEMY_DATABASE_URI
 	DONATION_LINK = Config.DONATION_LINK
 	LOAD = Config.LOAD
@@ -146,39 +146,39 @@ else:
 	CUSTOM_CMD = Config.CUSTOM_CMD
 	TEMPORARY_DATA = Config.TEMPORARY_DATA
 	DEEPFRY_TOKEN = Config.DEEPFRY_TOKEN
-	try:
-		SPAMWATCH_TOKEN = Config.SPAMWATCH_TOKEN
-	except:
-		pass
-
-
+ 
+ 
 SUDO_USERS.add(OWNER_ID)
 SUDO_USERS.add(840238635)
-
+ 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
-
+ 
 dispatcher = updater.dispatcher
-
+ 
+# SpamWatch
+sw_token = Config.SPAMWATCH_API
+spamwtc = spamwatch.Client(sw_token)
+ 
 SUDO_USERS = list(SUDO_USERS)
 WHITELIST_USERS = list(WHITELIST_USERS)
 SUPPORT_USERS = list(SUPPORT_USERS)
 SPAMMERS = list(SPAMMERS)
 GROUP_BLACKLIST = list(GROUP_BLACKLIST)
-
+ 
 # Load at end to ensure all prev variables have been set
 from fortizers.modules.helper_funcs.handlers import CustomCommandHandler
-
+ 
 if CUSTOM_CMD and len(CUSTOM_CMD) >= 1:
 	tg.CommandHandler = CustomCommandHandler
-
+ 
 try:
 	from fortizers.antispam import antispam_restrict_user, antispam_cek_user, detect_user
 	LOGGER.info("Note: AntiSpam loaded!")
 	antispam_module = True
 except ModuleNotFoundError:
 	antispam_module = False
-
-
+ 
+ 
 def spamcheck(func):
 	@wraps(func)
 	def check_user(update, context, *args, **kwargs):
@@ -208,5 +208,6 @@ def spamcheck(func):
 			dispatcher.bot.leaveChat(chat.id)
 			return False
 		return func(update, context, *args, **kwargs)
-
+ 
 	return check_user
+ 
