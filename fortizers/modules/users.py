AyraHikari/EmiliaHikari

Remove customfilter because of deprecated

emilia/modules/users.py
@@ -9,7 +9,7 @@ from telegram.ext import MessageHandler, Filters, CommandHandler
 from telegram.ext.dispatcher import run_async
 
 import emilia.modules.sql.users_sql as sql
-from emilia import dispatcher, OWNER_ID, LOGGER
+from emilia import dispatcher, OWNER_ID, LOGGER, SUDO_USERS
 from emilia.modules.helper_funcs.filters import CustomFilters
 
 import emilia.modules.sql.feds_sql as fedsql
@@ -139,7 +139,7 @@ __mod_name__ = "Users"
 
 BROADCAST_HANDLER = CommandHandler("broadcast", broadcast, filters=Filters.user(OWNER_ID))
 USER_HANDLER = MessageHandler(Filters.all & Filters.group, log_user)
-CHATLIST_HANDLER = CommandHandler("chatlist", chats, filters=CustomFilters.sudo_filter)
+CHATLIST_HANDLER = CommandHandler("chatlist", chats, filters=Filters.user(SUDO_USERS))
 
 dispatcher.add_handler(USER_HANDLER, USERS_GROUP)
 dispatcher.add_handler(BROADCAST_HANDLER)

fortifying/fortizerswatcher

Change-Id: I3a8ef36c12c3d472e0c661ea5e2181f0f0f9bad6

fortizers/modules/users.py
@@ -9,8 +9,7 @@ from telegram.ext import MessageHandler, Filters, CommandHandler
 from telegram.ext.dispatcher import run_async
 
 import fortizers.modules.sql.users_sql as sql
-from fortizers import dispatcher, OWNER_ID, LOGGER
-from fortizers.modules.helper_funcs.filters import CustomFilters
+from fortizers import dispatcher, OWNER_ID, LOGGER, SUDO_USER
 
 import fortizers.modules.sql.feds_sql as fedsql
 from fortizers.modules import languages
@@ -154,7 +153,7 @@ __mod_name__ = "Users"
 
 BROADCAST_HANDLER = CommandHandler("broadcast", broadcast, filters=Filters.user(OWNER_ID))
 USER_HANDLER = MessageHandler(Filters.all & Filters.group, log_user)
-CHATLIST_HANDLER = CommandHandler("chatlist", chats, filters=CustomFilters.sudo_filter)
+CHATLIST_HANDLER = CommandHandler("chatlist", chats, filters=Filters.user(SUDO_USER))
 
 dispatcher.add_handler(USER_HANDLER, USERS_GROUP)
 dispatcher.add_handler(BROADCAST_HANDLER)